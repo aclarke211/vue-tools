174d1adfcbe658b0d5815fb26ae92c5767623403
Alex Clarke <aclarke211@gmail.com>
2019-03-13 01:16:40 +0000

    Show formatted component names as route links in Home vue.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,38 @@
 <template>
   <div class="home">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
-
-
     <div class="component-list">
-      <h1>Select a Page</h1>
+      <h1>Components</h1>
       <p>Available pages:</p>
       <ul>
         <li
           v-for="componentRoute in componentRoutes"
           :key="componentRoute.path">
-          <router-link :to="componentRoute.path">{{ componentRoute.path }}</router-link>
+          <router-link
+            :to="componentRoute.path">
+            {{ formattedName(componentRoute.name) }}
+          </router-link>
         </li>
       </ul>
     </div>
-
-    <!-- <div class="link" v-for="(vueRoute, routeKey) in vueRoutes" :key="routeKey">
-      {{ 'Hey' }}
-    </div> -->
-
-    <FormModule/>
   </div>
 </template>
 
 <script>
-// import Vue from 'vue';
 import { routes } from '../router';
-import FormModule from '@/components/modules/FormModule.vue';
-
 
 export default {
   name: 'home',
-  components: {
-    FormModule,
-  },
   data: () => ({
     routes,
   }),
   computed: {
     componentRoutes() {
       return routes.filter(routeName => routeName.path.includes('/'));
+    },
+  },
+  methods: {
+    formattedName(name) {
+      return (name.charAt(0).toUpperCase() + name.slice(1)).match(/[A-Z][a-z]+|[0-9]+/g).join(' ');
     },
   },
 };
