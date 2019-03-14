e0a78a81679f61651ec299d571d8a3be3e5fb0cf
Alex Clarke <aclarke211@gmail.com>
2019-03-14 22:03:33 +0000

    Create route for TextModule.

---
+++ src/components/modules/TextModule.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="['vc', className]">
+    <p :class="['text', className]">Text</p>
+    <p :class="['subtext', className]">Subtext</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextModule',
+  data: () => ({
+    className: 'text-module',
+  }),
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -1,23 +1,39 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-const routes = [
+const generalRoutes = [
   {
     path: '*',
     name: 'home',
     component: () => import(/* webpackChunkName: "Home" */ './views/Home.vue'),
   },
+];
+
+const moduleRoutes = [
+  {
+    path: '/modules/general/text-module',
+    name: 'textModule',
+    component: () => import(/* webpackChunkName: "TextModule" */ './components/modules/TextModule.vue'),
+  },
+];
+
+const componentRoutes = [
   {
     path: '/components/general/form',
     name: 'formComponent',
     component: () => import(/* webpackChunkName: "FormComponent" */ './components/FormComponent.vue'),
   },
+];
+
+const utilRoutes = [
   {
     path: '/utils/styling/manage-stylesheets',
     name: 'manageStylesheets',
     component: () => import(/* webpackChunkName: "manageStylesheets" */ './components/utils/examples/manageStylesheets.vue'),
   },
 ];
+
+const routes = [...generalRoutes, ...moduleRoutes, ...componentRoutes, ...utilRoutes];
 
 Vue.use(Router);
 
