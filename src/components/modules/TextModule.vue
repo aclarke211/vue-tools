0b83bdf0eb206120a60539bacda2416807c7a357
Alex Clarke <aclarke211@gmail.com>
2019-03-15 01:33:10 +0000

    Created Text Module.

--- src/components/modules/TextModule.vue
+++ src/components/modules/TextModule.vue
@@ -1,15 +1,65 @@
 <template>
-  <div :class="['vc', className]">
-    <p :class="['text', className]">Text</p>
-    <p :class="['subtext', className]">Subtext</p>
+  <div
+    :class="['vc', className]">
+    <p
+    class="text"
+    :style="[initialTextStyles, textStyles]">
+      {{ text }}
+    </p>
+    <p
+    class="subtext"
+    :style="[initialSubtextStyles, subtextStyles]">
+      {{ subtext}}
+    </p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'TextModule',
+
   data: () => ({
     className: 'text-module',
   }),
+
+  computed: {
+    initialTextStyles() {
+      return {
+        'font-size': '1rem',
+        margin: 0,
+        padding: 0,
+      };
+    },
+
+    initialSubtextStyles() {
+      return {
+        'font-size': '.75rem',
+        margin: 0,
+        padding: 0,
+      };
+    },
+  },
+
+  props: {
+    text: {
+      type: String,
+      default: 'Text Module',
+    },
+
+    subtext: {
+      type: String,
+      default: 'Subtext Module',
+    },
+
+    textStyles: {
+      type: Object,
+      default: () => {},
+    },
+
+    subtextStyles: {
+      type: Object,
+      default: () => {},
+    },
+  },
 };
 </script>
