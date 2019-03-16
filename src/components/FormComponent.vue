0f58c51c46b15e41815b9deb2c85750023a1c6a8
Alex Clarke <aclarke211@gmail.com>
2019-03-16 22:18:03 +0000

    Apply different styles to text module based on width of the window.

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,11 +1,42 @@
 <template>
   <div class="vc form">
     <h1>This is a Form Component.</h1>
+    <TextModule :text="'This is the text'" :textStyles="formTextStyles"  />
+
+    <WindowSize  :showValues="true" @dimensions="(event) => { this.windowDimensions = event }" />
   </div>
 </template>
 
 <script>
+import TextModule from '@/components/modules/TextModule.vue';
+import WindowSize from '@/components/modules/logic/WindowSize.vue';
+
 export default {
   name: 'FormComponent',
+
+  data: () => ({
+    windowDimensions: {},
+  }),
+
+  components: {
+    TextModule,
+    WindowSize,
+  },
+
+  computed: {
+    formTextStyles() {
+      if (this.windowDimensions.width <= 767) {
+        return {
+          color: 'red',
+          'font-size': '2rem',
+        };
+      }
+
+      return {
+        color: 'blue',
+        'font-size': '1.5rem',
+      };
+    }
+  }
 };
 </script>
