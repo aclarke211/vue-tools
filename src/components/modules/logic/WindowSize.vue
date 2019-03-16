99c215fc6bda5c0ed35a64d2a4a0924a662fbe14
Alex Clarke <aclarke211@gmail.com>
2019-03-16 20:22:26 +0000

    WindowSize module calculates window width.

---
+++ src/components/modules/logic/WindowSize.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="window-size__container">
+    <h1>Window Size Module</h1>
+    <h2>{{ windowWidth }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WindowSize',
+
+  data: () => ({
+    windowWidth: undefined,
+    windowHeight: undefined,
+  }),
+
+  mounted() {
+    window.addEventListener('resize', this.handleResize);
+    this.handleResize();
+  },
+
+  methods: {
+    handleResize() {
+      this.$nextTick(() => {
+        this.windowWidth = window.innerWidth;
+      });
+    },
+  },
+
+  beforeDestroy() {
+    window.removeEventListener('resize', this.handleResize);
+  },
+};
+</script>
