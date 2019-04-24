d053a8fea6a450f0cf8a740cfa6405f1d51fe511
Alex Clarke <aclarke211@gmail.com>
2019-04-25 00:01:55 +0100

    Set Home route.

--- src/router.js
+++ src/router.js
@@ -1,61 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-const generalRoutes = [
-  {
-    path: '*',
-    name: 'home',
-    component: () => import(/* webpackChunkName: "Home" */ './views/Home.vue'),
-  },
-];
-
-const moduleRoutes = [
-  {
-    path: '/modules/general/text-module',
-    name: 'textModule',
-    component: () => import(/* webpackChunkName: "TextModule" */ './components/modules/TextModule.vue'),
-  },
-  {
-    path: '/modules/logic/window-size',
-    name: 'windowSize',
-    component: () => import(/* webpackChunkName: "windowSize" */ './components/modules/WindowSize.vue'),
-  },
-  {
-    path: '/modules/images/picture',
-    name: 'pictureModule',
-    component: () => import(/* webpackChunkName: "windowSize" */ './components/modules/PictureModule.vue'),
-  },
-];
-
-const componentRoutes = [
-  {
-    path: '/components/general/mobile-desktop-styles-examples',
-    name: 'mobileDesktopStylesExample',
-    component: () => import(/* webpackChunkName: "MobileDesktopStylesExample" */ './components/MobileDesktopStylesExample.vue'),
-  },
-];
-
-const utilRoutes = [
-  {
-    path: '/utils/styling/manage-stylesheets',
-    name: 'manageStylesheets',
-    component: () => import(/* webpackChunkName: "manageStylesheets" */ './components/utils/examples/manageStylesheets.vue'),
-  },
-  {
-    path: '/utils/scripts/debounce-throttle',
-    name: 'debounceThrottle',
-    component: () => import(/* webpackChunkName: "debounceThrottle" */ './components/utils/examples/debounceThrottle.vue'),
-  },
-];
-
-const routes = [...generalRoutes, ...moduleRoutes, ...componentRoutes, ...utilRoutes];
-
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import(/* webpackChunkName: "Home" */ './views/Home.vue'),
+    },
+  ],
 });
-
-export { routes };
