93627d5473e511f20a4d7052886d076401c7dd15
Alex Clarke <Alex.Clarke@jdplc.com>
2019-04-25 17:18:44 +0100

    File to add/remove stylesheets.

--- src/utils/manageStylesheets.js
+++ src/utils/manageStylesheets.js
@@ -3,6 +3,7 @@ const stylesheetsClassName = 'external-stylesheet';
 // Append an external stylesheet to a page
 export function appendStylesheet(stylesPath) {
   const link = document.createElement('link');
+
   link.rel = 'stylesheet';
   link.type = 'text/css';
   link.href = stylesPath;
@@ -10,6 +11,25 @@ export function appendStylesheet(stylesPath) {
   document.head.appendChild(link);
   // eslint-disable-next-line
   console.info(`%cAdded stylesheet: ${stylesPath}.`, 'font-size: .75rem; color: slategrey');
+}
+
+export function removeStylesheet(stylesPath) {
+  // eslint-disable-next-line
+  console.log('Removing Stylesheet');
+  // eslint-disable-next-line
+  console.log(stylesPath);
+
+  const sheet = document.querySelector(`link[href="${stylesPath}"][class="external-stylesheet"]`);
+
+  // eslint-disable-next-line
+  console.log(sheet);
+
+  try {
+    sheet.parentNode.removeChild(sheet);
+  } catch (err) {
+    // eslint-disable-next-line
+    console.info(`%cCould not remove stylesheet: ${sheet}.`, 'font-size: .75rem; color: slategrey');
+  }
 }
 
 export function removeAppendedStylesheets() {
