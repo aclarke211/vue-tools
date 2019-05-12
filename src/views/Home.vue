c0a455c4cd22da592a25f66a699c86ac59efc632
Alex Clarke <aclarke211@gmail.com>
2019-05-12 21:12:20 +0100

    Can pass the width of border through to hover-invert mixin.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -127,13 +127,11 @@ export default {
             @include flexbox;
 
             &-btn {
-              @include hover-invert(cornflowerblue, white);
+              @include hover-invert(cornflowerblue, white, 2px);
               @include flexbox;
-              border: 2px solid cornflowerblue;
               margin: 1rem;
               padding: .75rem 2rem;
               text-align: center;
-              transition: all .5s;
               flex: 1 0 auto;
             }
           }
