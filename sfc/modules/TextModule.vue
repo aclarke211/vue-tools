594a49ef28e861d470597414382160ac07af39ac
Alex Clarke <Alex.Clarke@jdplc.com>
2019-10-14 01:51:07 +0100

    Alignment prop in Text Module.

--- sfc/modules/TextModule.vue
+++ sfc/modules/TextModule.vue
@@ -1,7 +1,7 @@
 <template>
     <p
     v-if="text"
-    :class="[className, config.className]"
+    :class="[className, config.className, textAlignment]"
     v-html="text" />
 </template>
 
@@ -21,14 +21,55 @@ export default {
       type: String,
       default: 'Text Module',
     },
+    align: {
+      /**
+       * String or Object here so we have the option to
+      add the 'options' property, which can be used in a select box
+       */
+      type: [String, Object],
+      default: () => ({
+        current: 'left',
+        options: [
+          {
+            text: 'Option 1',
+            value: 'option-1',
+          },
+        ],
+      }),
+    },
+  },
+
+  computed: {
+    textAlignment() {
+      if (this.align.current) {
+        return this.align.current;
+      }
+      return this.align;
+    },
   },
 };
 </script>
 
 <style lang="scss">
 .text-module {
+  display: flex;
   font-size: 1rem;
   padding: 0;
   margin: 0;
+
+  &.left {
+      justify-content: flex-start;
+      align-items: flex-start;
+    }
+
+    &.center {
+      justify-content: center;
+      align-items: center;
+    }
+
+    &.right {
+      justify-content: flex-end;
+      align-items: flex-end;
+    }
 }
 </style>
